e7ffec2de611b64aea739a45ba2d1ad4c6406d14
Vincent <445047477@qq.com>
2019-02-01 09:38:39 +0800

    add web01

---
+++ src/test/java/Test01.java
@@ -0,0 +1,16 @@
+public class Test01 {
+
+    private  String name ="s";
+    public static void main(String[] args) {
+//        IDEA怎样使用git将项目上传到github中
+//
+//
+//
+//        https://blog.csdn.net/Larrywjx/article/details/78268266
+//        git config --global user.name "Vincent"
+//        git config --global user.email "445047477@qq.com"
+//        https://www.cnblogs.com/sxdcgaq8080/p/8058898.html
+
+
+    }
+}
