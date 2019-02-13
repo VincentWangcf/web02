7ecb9fab7edad5328f8b0d7bd681f20a1b8efa3a
Vincent <445047477@qq.com>
2019-02-13 13:35:07 +0800

    add 0213 for package

--- src/test/java/Feed.java
+++ src/test/java/Feed.java
@@ -12,10 +12,6 @@ class Feed implements Subject{
 
     public static void main(String[] args) {
         Feed f = new Feed();
-        f.registerObserver(new NYTimes());
-        f.registerObserver(new Guardian());
-        f.registerObserver(new LeMonde());
-        f.notifyObservers("The queen said her favourite book is Java 8 in Action!");
         f.registerObserver((String tweet) -> {
             if(tweet != null && tweet.contains("money")){
                 System.out.println("Breaking news in NY! " + tweet);
@@ -26,5 +22,10 @@ class Feed implements Subject{
                 System.out.println("Yet another news in London... " + tweet);
             }
         });
+//        f.registerObserver(new NYTimes());
+//        f.registerObserver(new Guardian());
+//        f.registerObserver(new LeMonde());
+        f.notifyObservers("The queen said her favourite book is Java 8 in Action!");
+
     }
 }
