b12358ddbd7ff976748ff12fc1126cf139a9dc60
Vincent <445047477@qq.com>
2019-02-13 15:22:41 +0800

    b add

---
+++ src/test/java/java8/Insurance.java
@@ -0,0 +1,11 @@
+package java8;
+
+public class Insurance {
+    //baoxiang  保险
+    private String name;
+    public String getName() { return name; }
+    public void setName(String name){
+        this.name=name;
+    }
+
+}
